c840a077cf2dc9ddcafa9d3f8b2d4dc7f0e0b0db
So May See Macy <so._.may._.see._.macy@Sode-MBP.fios-router.home>
2021-01-06 13:48:00 -0500

    35. Search Insert Position

---
+++ 35.Search.Insert.Position.cpp
@@ -0,0 +1,49 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+/*
+ Input: nums = [1,3,5,6], target = 5
+ Output: 2
+
+ Input: nums = [1,3,5,6], target = 2
+ Output: 1
+ 
+ Input: nums = [1,3,5,6], target = 7
+ Output: 4
+ */
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        if (n < 1){
+            return 0;
+        }
+        int low = 0;
+        int high = n - 1;
+        int mid;
+        int pos;
+        while (low <= high){
+            mid = low + (high - low) / 2;
+            if (nums[mid] == target){
+                return mid;
+            }
+            else if (nums[mid] > target){
+                high = mid - 1;
+                pos = mid;
+            }
+            else{
+                low = mid + 1;
+                pos = mid + 1;
+            }
+        }
+        return pos;
+    }
+};
+
+int main() {
+
+    return 0;
+}
